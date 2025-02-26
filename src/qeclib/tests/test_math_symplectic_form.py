08c05e4eaced68022b1bb609db9a311a89693a5a
Jonathan Knoll <jonknoll@mit.edu>
2025-02-26 17:08:06 -0500

    Rename file

--- src/qeclib/tests/test_math_symplectic_form.py
+++ src/qeclib/tests/test_math_symplectic_form.py
@@ -14,6 +14,7 @@ from qeclib.math import (
     cartesian_product_of_sets,
     get_log_op_distribution,
     F2_to_xyz,
+    stab_to_F2,
 )
 
 
@@ -582,6 +583,22 @@ class TestMath(unittest.TestCase):
         self.assertEqual(F2_to_xyz([0, 1, 0, 0]), "IX")
         self.assertEqual(F2_to_xyz([1, 0, 1, 1]), "YZ")
         self.assertEqual(F2_to_xyz([0, 1, 0, 0, 0, 0, 1, 0, 0, 0]), "IYIII")
+
+    def test_stab_to_F2(self):
+        stab_matrix = stab_to_F2(
+            [
+                ["XXI", [0, 1, None], 42],
+                ["IXX", [None, 1, 2], 42],
+                ["ZZZ", [0, 1, 2], 42],
+            ]
+        )
+        expected_stab_matrix = [
+            [1, 1, 0, 0, 0, 0],
+            [0, 1, 1, 0, 0, 0],
+            [0, 0, 0, 1, 1, 1],
+        ]
+        for i in range(len(stab_matrix)):
+            self.assertEqual(tuple(stab_matrix[i]), tuple(expected_stab_matrix[i]))
 
 
 if __name__ == "__main__":
